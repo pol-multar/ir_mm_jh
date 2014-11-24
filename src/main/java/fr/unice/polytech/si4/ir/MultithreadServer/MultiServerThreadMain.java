73604704cfdcc0df6e6655ec40e217e356c749c0
jinhong211 <jinhong211@gmail.com>
2014-11-24 15:49:04 +0100

    mulitiserverthread

--- src/main/java/fr/unice/polytech/si4/ir/MultithreadServer/MultiServerThreadMain.java
+++ src/main/java/fr/unice/polytech/si4/ir/MultithreadServer/MultiServerThreadMain.java
@@ -7,16 +7,38 @@ public class MultiServerThreadMain {
 	public static void main(String[] args) throws IOException{
 		ServerSocket serverSocket = null;
 		boolean listening = true;
-		try {
-			serverSocket = new ServerSocket(999);
-		}
-		catch (IOException e){
-			System.err.println("Je ne peux pas écouter le port:999");
-			System.exit(-1);
-		}
-		while (listening){
-			new MutiServerThread(serverSocket.accept()).start();
-			serverSocket.close();
-		}
+		System.out.println("Lancement de la méthode principale du serveur");
+		if(args.length==0) {
+            System.out.println("Le port n'a pas été spécifié, port par défaut: 9999");
+            System.out.println("Server : Lancement du serveur de l'annuaire");
+            try {
+    			serverSocket = new ServerSocket(9999);
+    		}
+    		catch (IOException e){
+    			System.err.println("Server : "+"Impossible d'ouvrir le port, erreur : " + e.toString());
+    			System.exit(-1);
+    		}
+    		while (listening){
+    			System.out.println("IP : " + serverSocket.getInetAddress() + " : " + serverSocket.getLocalPort());
+    			new MutiServerThread(serverSocket.accept()).run();
+    			serverSocket.close();
+    		}
+        }else{
+            System.out.println("Port spécifié, lancement sur le port indiqué : "+args[0]);
+            System.out.println("Server : Lancement du serveur de l'annuaire");
+            try {
+    			serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+    		}
+    		catch (IOException e){
+    			System.err.println("Server : "+"Impossible d'ouvrir le port, erreur : " + e.toString());
+    			System.exit(-1);
+    		}
+            while (listening){
+            	System.out.println("IP : " + serverSocket.getInetAddress() + " : " + serverSocket.getLocalPort());
+    			new MutiServerThread(serverSocket.accept()).run();
+    			serverSocket.close();
+    		}
+        }
+		System.out.println("Serveur : J'ai fini de travailler");
 	}
 }
--- src/main/java/fr/unice/polytech/si4/ir/MultithreadServer/MutiServerThread.java
+++ src/main/java/fr/unice/polytech/si4/ir/MultithreadServer/MutiServerThread.java
@@ -5,33 +5,156 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+import fr.unice.polytech.si4.ir.model.DirectoryData;
 
 public class MutiServerThread extends Thread{
-	private Socket socket = null;
 	private String line;
+    private Socket clientSocket;
+    private BufferedReader is;
+    private PrintWriter os;
+    private Boolean interOut;
+    private DirectoryData directoryData;
+    private Boolean verbose;
+    
 	public MutiServerThread(Socket socket){
 		super("MutiServerThread");
-		this.socket = socket;
+		this.clientSocket = socket;
+        line = null;
+        is = null;
+        os = null;
+        interOut = true;
+        directoryData = new DirectoryData();
+        verbose=true;
 	}
 	
 	public void run(){
 		try {
-			PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
-			BufferedReader in = new BufferedReader( new InputStreamReader(socket.getInputStream()));
-			while(true){
-                line=in.readLine();
-                out.println(line);
-                if(line.equals("OK")){
-                    System.out.println("J'ai reçu ok, Je ferme la connection");
+			os = new PrintWriter(clientSocket.getOutputStream(),true);
+			is = new BufferedReader( new InputStreamReader(clientSocket.getInputStream()));
+			while (interOut) {
+                line = is.readLine();
+                interOut = interpretor(line);
+            }
+            os.close();
+            is.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+	}
+	
+	private boolean interpretor(String line) {
+
+        String[] s = line.split(";");
+
+        switch (s[0]) {
+            case "EXIT":
+                printInfo("J'ai reçu un EXIT du client, Je ferme la connection");
+                os.println("EXITOK");
+                return false;
+            case "AD":
+                printInfo("Le client veut ajouter le nom : " + s[1]);
+                os.println("SURN?");
+                addName(s[1]);
+                return true;
+            case "PRINTSNAME":
+                printInfo("Le client veut afficher les surnoms associés à : " + s[1]);
+                printsname(s[1]);
+                return true;
+            default:
+                printErr("Instruction non reconnue : " + s[0]);
+                os.println(line);
+                return true;
+        }
+
+
+    }
+
+    /**
+     * Method in charge of adding a name and its nicknames in the directory
+     * @param s the name to add
+     */
+
+    private void addName(String s) {
+        String name = s;
+
+        try {
+            while(true){
+                line=is.readLine();
+                if("END".equals(line)){
+                    printInfo("Ajout terminé");
+                    os.println("ADOK");
                     break;
                 }
+                printInfo(name+" obtient un nouveau surnom : "+line);
+                directoryData.addEntry(name,line);
+                os.println("SURN?");
+
             }
-            out.close();
-            in.close();
-            socket.close();
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-	}
+
+        } catch (IOException e) {
+            printErr(e.toString());
+        }
+
+
+    }
+
+    /**
+     * Method in charge to print nicknames for a name
+     *
+     * @param name the name to associate
+     */
+    private void printsname(String name) {
+
+        ArrayList<String> nickList;
+
+        nickList = this.directoryData.getNick(name);
+        if (nickList.size() != 0) {
+            ListIterator<String> li = nickList.listIterator();
+
+            osPrinter(name + " est aussi appelé : ");
+
+            while (li.hasNext()) {
+                osPrinter(li.next());
+            }
+
+
+        } else {
+            osPrinter(name + " n'a pas de surnom ");
+        }
+
+    }
+
+    /**
+     * Method in charge of printing strings on the Server outputstream
+     *
+     * @param s the string to send
+     */
+    private void osPrinter(String s) {
+        os.println(s);
+    }
+
+    /**
+     * Method in charge of displaying messages
+     * @param s
+     */
+    private void printInfo(String s) {
+
+        if (verbose) {
+            System.out.println("Server : "+s);
+        }
+
+    }
+
+    /**
+     * Method in charge of displaying errors
+     *
+     * @param s string to display with the error
+     */
+    private void printErr(String s) {
+        System.err.println("Server : "+s);
+    }
 }
